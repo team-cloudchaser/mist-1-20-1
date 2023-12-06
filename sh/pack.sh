23d123e4cd7928aa23fbebc7cd47b7830af740c7
Lumière Élevé <88174309+PoneyClairDeLune@users.noreply.github.com>
2023-12-06 02:01:35 +0000

    Bootstrap the options.

--- sh/pack.sh
+++ sh/pack.sh
@@ -10,7 +10,7 @@ echob "Creating root ZIP archives..."
 zip -r0v build/baseline.zip \
 	HMCL-3.5.5.jar
 zip -r9v build/baseline.zip \
-	LICENSE README.md gpuEnforce.sh toTheDir.sh \
+	LICENSE README.md gpuEnforce.sh toTheDir.sh .hmcl.json \
 	.minecraft/launcher_profiles.json
 cp build/baseline.zip build/mainline.zip
 echob "Bundling the Baseline pack..."
