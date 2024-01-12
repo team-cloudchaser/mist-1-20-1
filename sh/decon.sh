d313fddfc5c977456ef2d10a32be7842db60d023
Lumière Élevé <88174309+PoneyClairDeLune@users.noreply.github.com>
2024-01-12 12:14:51 +0000

    Decon is now specific to declared paths.

--- sh/decon.sh
+++ sh/decon.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 # Removes identifying info from certain auto-generated files. (decontamination)
 genDate="#$(date '+%a %b %d %H:%M:%S %Z %Y')"
-echo $genDate
+cat conf/decon.conf | while IFS= read -r file; do
+	if [[ "$file" != "#"* ]] ; then
+		printf "Decontaminating \"$file\"... "
+		echo "done."
+	fi
+done
 exit
