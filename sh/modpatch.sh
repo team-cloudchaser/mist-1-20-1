7e8929ff7f5aad29af2077e9ded1d225bb1da039
Lumière Élevé <88174309+PoneyClairDeLune@users.noreply.github.com>
2024-01-19 11:31:53 +0000

    Automatic mod data patcher.

---
+++ sh/modpatch.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+patch="data/dehydration/hydration_items"
+cd ".minecraft/versions/1.20.1 Fabric Mainline/mods"
+zip -r9 "dehydration-1.3.5.jar" "${patch}/vanilla_items.json"
+zip -r9 "Unicopia-1.2.2+1.20.1.jar" "${patch}/unicopia_food.json"
+zip -r9 "midas-hunger-2.2.1+1.20.jar" "${patch}/midas_food.json"
+zip -r9 "culinaire-3.0.0.jar" "${patch}/culinaire_food.json"
+zip -r9 "promenade-4.1.1.jar" "${patch}/promenade_food.json"
+zip -r9 "farmers-delight-fabric-1.4.3.jar" "${patch}/delight_food.json"
+zip -r9 "ubes-delight-0.1.5b.jar" "${patch}/ube_delight_food.json"
+zip -r9 "letsdo-vinery-1.4.7-fabric.jar" "${patch}/vinery_food.json"
+zip -r9 "letsdo-nethervinery-fabric-2.0.2.jar" "${patch}/nether_vinery_food.json"
+exit
